23fce4385af5068d046a397d6a2f7ff1d6ed51ae
CeciliaColley <ceciliagabriellecolley@gmail.com>
2024-05-18 17:38:27 -0300

    movement with a character is so much better now.

--- Assets/Scripts/Input/FirstPersonController.cs
+++ Assets/Scripts/Input/FirstPersonController.cs
@@ -1,102 +1,69 @@
-using System.Threading;
-using UnityEditor.Rendering;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Scripting.APIUpdating;
-using Input = UnityEngine.Input;
-
-// Find documentation on this script in the documentation folder, under the file name "FirstPersonController.txt"
 
 [RequireComponent(typeof(CharacterController))]
-public class FirstPersonController : MonoBehaviour
+public class FPSController : MonoBehaviour
 {
     IA_PlayerActions playerActions;
-    private CharacterController controller;
+    private Vector2 movementInput = Vector2.zero;
+    private Vector2 lookInput = Vector2.zero;
+    private float movementDirectionY;
+    private bool isRunning = false;
+    private bool jumped = false;
 
+    public Camera playerCamera;
+    public float walkSpeed = 6f;
+    public float runSpeed = 12f;
+    public float jumpPower = 7f;
+    public float gravity = 10f;
 
-    [Header("Grounded Variables")]
-    [Tooltip("If the character is grounded or not. Not part of the CharacterController built in grounded check")]
-    public bool grounded = true;
-    [Tooltip("Offset to mark feet position")]
-    public float groundedOffset = 0.85f;
-    [Tooltip("The radius of the grounded check. Should match the radius of the CharacterController")]
-    public float groundedRadius = 0.5f;
-    [Tooltip("What layers the character uses as ground")]
-    public LayerMask groundLayers;
+    public float lookSpeed = 2f;
+    public float lookXLimit = 45f;
 
-    [Space(10)]
-    [Header("Jump Variables")]
-    [Tooltip("The height the player can jump")]
-    public float jumpHeight = 1.2f;
-    [Tooltip("The character uses its own gravity value. The engine default is -9.81f")]
-    public float gravity = -15.0f;
-    public float terminalVelocity = 53.0f;
-    [Tooltip("The fastest the character can fall")]
-    public float maxFallSpeed = -50.0f;
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
 
-    [Space(10)]
-    [Header("Movement Variables")]
-    [Tooltip("Move speed of the character in m/s")]
-    public float moveSpeed = 6.0f;
-    [Tooltip("Sprint speed of the character in m/s")]
-    public float sprintSpeed = 10.0f;
-    [Tooltip("Rotation speed of the character")]
-    public float rotationSpeed = 2.0f;
-    public Transform look;
+    public bool canMove = true;
 
-    [Space(10)]
-    [Header("Camera Limits")]
-    public float minCameraAngle = -90F;
-    public float maxCameraAngle = 90F;
-
-
-    private float verticalVelocity;
-    private Vector3 movementVector;
-    private Vector3 updatedDirection;
-    private Vector3 direction;
-    private float targetSpeed;
-
-    private Quaternion characterTargetRot;
-    private Quaternion cameraTargetRot;
-
-
-    private void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        controller = GetComponent<CharacterController>();
-        characterTargetRot = transform.localRotation;
-        cameraTargetRot = look.localRotation;
-    }
+    CharacterController characterController;
 
     private void Awake()
     {
         playerActions = new IA_PlayerActions();
     }
 
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
     private void OnEnable()
     {
         playerActions.PlayerActions.Enable();
-        
+
         playerActions.PlayerActions.Jump.performed += ctx => Jump();
-        
+
         playerActions.PlayerActions.Move.performed += ctx => onMovementPerformed(ctx);
-        playerActions.PlayerActions.Move.canceled += ctx => onMovementCanceled();
+        playerActions.PlayerActions.Move.canceled += ctx => onMovementCanceled(ctx);
 
         playerActions.PlayerActions.Sprint.performed += ctx => onSprintPerformed();
         playerActions.PlayerActions.Sprint.canceled += ctx => onSprintCanceled();
 
         playerActions.PlayerActions.Look.performed += ctx => LookRotation(ctx);
-
     }
 
     private void OnDisable()
     {
         playerActions.PlayerActions.Disable();
-        
+
         playerActions.PlayerActions.Jump.performed -= ctx => Jump();
-        
+
         playerActions.PlayerActions.Move.performed -= ctx => onMovementPerformed(ctx);
-        playerActions.PlayerActions.Move.canceled -= ctx => onMovementCanceled();
+        playerActions.PlayerActions.Move.canceled -= ctx => onMovementCanceled(ctx);
 
         playerActions.PlayerActions.Sprint.performed -= ctx => onSprintPerformed();
         playerActions.PlayerActions.Sprint.canceled -= ctx => onSprintCanceled();
@@ -104,104 +71,71 @@ public class FirstPersonController : MonoBehaviour
         playerActions.PlayerActions.Look.performed -= ctx => LookRotation(ctx);
     }
 
-    private void Update()
+    void Update()
     {
-        ApplyGravity();
-        controller.Move(movementVector * (targetSpeed * Time.deltaTime) + new Vector3(0.0f, verticalVelocity, 0.0f) * Time.deltaTime);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        // Press Left Shift to run
+        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * movementInput.y : 0;
+        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * movementInput.x : 0;
+        movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        if (jumped && canMove && characterController.isGrounded)
+        {
+            moveDirection.y = jumpPower;
+            jumped = false;
+        }
+        else
+        {
+            moveDirection.y = movementDirectionY;
+        }
+
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+        characterController.Move(moveDirection * Time.deltaTime);
     }
 
-    private void ApplyGravity()
+    void Jump()
     {
-        // Stop increasing veritcal velocity when maximum fall speed is reached
-        if (verticalVelocity > maxFallSpeed)
+        jumped = true;
+        
+    }
+
+    void onMovementPerformed(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+    void onMovementCanceled(InputAction.CallbackContext context)
+    {
+        movementInput = Vector2.zero;
+    }
+
+    void onSprintPerformed()
+    {
+        isRunning = true;
+    }
+
+    void onSprintCanceled()
+    {
+        isRunning = false;
+    }
+
+    void LookRotation(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+
+        if (canMove)
         {
-            // apply gravity over time if under terminal (multiply by delta time twice to linearly speed up over time)
-            if (verticalVelocity < terminalVelocity)
-            {
-                verticalVelocity += gravity * Time.deltaTime;
-            }
+            rotationX += -lookInput.y * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, lookInput.x * lookSpeed, 0);
         }
     }
-
-    private void Jump() 
-    {
-        // Check if grounded
-        Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - groundedOffset, transform.position.z);
-        grounded = Physics.CheckSphere(spherePosition, groundedRadius, groundLayers, QueryTriggerInteraction.Ignore);
-
-        if (grounded)
-        {
-            // The square root of H * -2 * G = how much velocity needed to reach desired height
-            verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
-    }
-
-    private void onMovementPerformed(InputAction.CallbackContext ctx)
-    {
-        // Register the basic direction (forward, backward, left, right)
-        Vector2 horizontalVector = ctx.ReadValue<Vector2>();
-        direction = new Vector3(horizontalVector.x, 0, horizontalVector.y);
-
-        // Set speed
-        targetSpeed = moveSpeed;
-
-        SetMovementDirection();
-    }
-
-    private void onMovementCanceled()
-    {
-        // Stop moving
-        movementVector = Vector3.zero;
-
-        //Reset direction
-        direction = Vector3.zero;
-    }
-
-    private void SetMovementDirection()
-    {
-        // Establish "forward" as the direction the camera is facing
-        updatedDirection = direction.x * transform.right + direction.z * transform.forward;
-        movementVector = updatedDirection.normalized;
-    }
-
-    private void onSprintPerformed()
-    {
-        targetSpeed = sprintSpeed;
-    }
-
-    private void onSprintCanceled()
-    {
-        targetSpeed = moveSpeed;
-    }
-
-    public void LookRotation(InputAction.CallbackContext ctx)
-    {
-        Vector2 lookVector = ctx.ReadValue<Vector2>();
-        float yRot = lookVector.x * rotationSpeed;
-        float xRot = lookVector.y * rotationSpeed;
-
-        characterTargetRot *= Quaternion.Euler(0f, yRot, 0f);
-        cameraTargetRot *= Quaternion.Euler(-xRot, 0f, 0f);
-
-        cameraTargetRot = ClampRotationAroundXAxis(cameraTargetRot);
-        transform.localRotation = characterTargetRot;
-        look.localRotation = cameraTargetRot;
-
-        SetMovementDirection();
-    }
-
-    Quaternion ClampRotationAroundXAxis(Quaternion q)
-    {
-        q.x /= q.w;
-        q.y /= q.w;
-        q.z /= q.w;
-        q.w = 1.0f;
-
-        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
-        angleX = Mathf.Clamp(angleX, minCameraAngle, maxCameraAngle);
-
-        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
-
-        return q;
-    }
-}    
+}
--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class PlayerState : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private bool hasGun = false;
+    [SerializeField] private bool canFish = false;
 }
